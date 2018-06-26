ce2b155a14ea2376aa401dcf75422c1e44ec5123
tanvir <tanvir.alam.shawn@gmail.com>
2018-06-26 22:22:55 +0200

    3. Make an API routes with controller to retrieve, update and delete options

---
+++ app/Http/Controllers/Api/OptionsController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use App\Option;
+
+class OptionsController extends Controller
+{
+    /**
+     * Show all Options
+     *
+     * @return Array
+     */
+    public function index()
+    {
+        return Option::all();
+    }
+
+    /**
+     * Show single option
+     *
+     * @param int $id
+     * 
+     * @return array
+     */
+    public function show($id)
+    {
+        $option = Option::find($id);
+
+        if (is_null($option)) {
+            return $this->sendError('Option not found.');
+        }
+        return $this->sendResponse($option->toArray(), 'Option retrieved successfully.');
+    }
+
+    /**
+     * Save options with proper validation
+     *
+     * @param Request $request
+     * 
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'key' => 'required',
+            'value' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $option = Option::create($request->all());
+
+        return response()->json($option, 201);
+    }
+
+    /**
+     * Update options
+     *
+     * @param Request $request
+     * @param Option $option
+     * 
+     * @return void
+     */
+    public function update(Request $request, Option $option)
+    {
+        $option->update($request->all());
+
+        return response()->json($option, 200);
+    }
+
+    /**
+     * Delete options
+     *
+     * @param Option $option
+     * 
+     * @return void
+     */
+    public function delete(Option $option)
+    {
+        $option->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('options', 'Api\OptionController@index');
+    Route::get('options/{option}', 'Api\OptionController@show');
+    Route::post('options', 'Api\OptionController@store');
+    Route::put('options/{option}', 'Api\OptionController@update');
+    Route::delete('options/{option}', 'Api\OptionController@delete');
 });
